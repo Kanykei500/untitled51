2a3916743dfa982fc6726dedcf957e416d3cf600
Kanykei500 <askarbekovakanykei120@gmail.com>
2022-11-30 12:28:21 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while (true){
+            try {
+                Scanner scanner=new Scanner(System.in);
+
+                Parallelepiped parallelepiped=new Parallelepiped (scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+                System.out.println(parallelepiped);
+                parallelepiped.getArea();
+                parallelepiped.getVolume();
+            } catch (ArithmeticException e){
+                System.out.println("number");
+            }catch (Exception e){
+                System.out.println("nepravilno");
+            }
+
+        }
+
     }
 }
--- src/Parallelepiped.java
+++ src/Parallelepiped.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Parallelepiped {
+public class Parallelepiped {
+    private int dlina;
+    private int vysota;
+    private int shirina;
+
+    public Parallelepiped(){
+
+    }
+
+    public Parallelepiped(int dlina, int vysota, int shirina) throws Exception{
+
+            chekInfo(dlina,vysota,shirina);
+        }
+
+
+    private void chekInfo(int dlina,int vysota,int shirina)throws Exception{
+        if (dlina<0 || shirina<0 || vysota<0 || dlina>20 || shirina>20 || vysota>20 ){
+            throw new Exception();
+        } else {
+            this.dlina=dlina;
+            this.vysota=vysota;
+            this.shirina=shirina;
+        }
+    }
+
+    public int getDlina() {
+        return dlina;
+    }
+
+    public void setDlina(int dlina) {
+        this.dlina = dlina;
+    }
+
+    public int getVysota() {
+        return vysota;
+    }
+
+    public void setVysota(int vysota) {
+        this.vysota = vysota;
+    }
+
+    public int getShirina() {
+        return shirina;
+    }
+
+    public void setShirina(int shirina) {
+        this.shirina = shirina;
+    }
+    public void getArea(){
+        System.out.println(2*((vysota * dlina) + (dlina * shirina) + (vysota * shirina)));
+    }
+    public void getVolume(){
+        System.out.println(dlina*shirina*vysota);
+    }
+
+    @Override
+    public String toString() {
+        return "Parallelepiped{" +
+                "dlina=" + dlina +
+                ", vysota=" + vysota +
+                ", shirina=" + shirina +
+                '}';
+    }
 }
